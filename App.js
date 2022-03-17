d7e5e812cc0936950e2eea3fd185e09d09e0e753
arun ps <psarun2015@gmail.com>
2022-03-17 13:46:23 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { View,StyleSheet,Text}
+ from 'react-native'
+ export default function App(){
+   return(
+     <View style={styles.container}> 
+       <Text>hgfgfe</Text>
+     </View>
+   )
+ }
+ const styles=StyleSheet.create({
+   container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+   }
+ })
